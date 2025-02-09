4287b6acde417439f5195017176f17a840a09ebc
sergio22 <mr.sergiolooolll@gmail.com>
2025-02-09 15:10:09 -0600

    Add files via upload

--- gct.js
+++ gct.js
@@ -68,12 +68,25 @@ function main(){
         let label_panel = document.querySelector('#label_panel');
         label_panel.style.display = "block";
     }
+    function valid(n){
+        if(Number(n) > 0 && Number(n) < 100){
+            return 1;    
+        }
+        return 0;
+    }
     function read_edge(){
         let nodeA = document.querySelector("#nodeA");
         let nodeB = document.querySelector("#nodeB");
         
         let u = nodeA.value;
         let v = nodeB.value;
+        
+        if(!valid(u)){
+            return;    
+        }
+        if(!valid(v)){
+            return;    
+        }
         
         if(!node_list[u]){
             create_node(u);
@@ -87,7 +100,10 @@ function main(){
     function read_name(){
         let input = document.querySelector("#idnode");
         let name = input.value;
-        create_node(name);
+        
+        if(valid(name) && !node_list[name]){
+            create_node(name);    
+        }
     }
     function create_line(x1, y1, x2, y2){
         requestAnimationFrame(create_line)
@@ -98,9 +114,6 @@ function main(){
     }
     function create_edge(u, v){
         edges.push([u, v]);
-        if(!node_list[u]){
-            create_node();
-        }
         make_edge(u, v);
     }
     function make_edge(u, v){
@@ -112,8 +125,8 @@ function main(){
         create_line(posU.left + 25, posU.top + 25, posV.left + 25, posV.top + 25);
     }
     new_node.addEventListener('click', config_node)
-    new_edge.addEventListener('click', config_edge);
-    send_edge.addEventListener('click', read_edge);
+    new_edge.addEventListener('click', config_edge)
+    send_edge.addEventListener('click', read_edge)
     send_node.addEventListener('click', read_name)
 }
 
